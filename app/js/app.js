e8e8d110cca3dbb012ef9edd3bb567af2d50386a
Aaron Chiandet <achiandet@gmail.com>
2016-02-07 13:47:01 -0700

    Installed tether to remove console errors

--- app/js/app.js
+++ app/js/app.js
@@ -2,6 +2,3 @@ console.log("Good job, I'm working");
 console.log("Keep it up");
 console.log("killing the game");
 console.log("Still doing it");
-
-document.getElementById('header').innerHTML =
-"The rillest dillest billest";
